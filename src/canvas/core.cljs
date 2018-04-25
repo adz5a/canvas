45175b8be85e88e792325d6a0e31d389259f6e14
adz5a <antoine.d.zimmermann@gmail.com>
2018-04-25 13:58:36 +0200

    [fn] line-to!

--- src/canvas/core.cljs
+++ src/canvas/core.cljs
@@ -3,20 +3,57 @@
 
 (enable-console-print!)
 
-(println "This text is printed from src/canvas/core.cljs. Go ahead and edit it and see reloading in action.")
+(def height 500)
+(def width 500)
 
-;; define your app data so that it doesn't get over-written on reload
+(defonce app-state (atom {:text "Hello world!"
+                          :!ref nil}))
 
-(defonce app-state (atom {:text "Hello world!"}))
+(def canvas (reagent/create-class {:component-did-mount (fn []
+                                                          (swap! app-state assoc :ctx (.getContext (:!ref @app-state)
+                                                                                                      "2d")))
+                             :reagent-render (fn []
+                                               [:canvas 
+                                                {:ref (partial swap! app-state assoc :!ref)
+                                                 :width width
+                                                 :height height
+                                                 :style {:border "black solid 1px"
+                                                         }}])}))
 
+(def origin [0 0])
+
+(defn set-transform! [ctx X Y D]
+  (let [[a b c d e f] (concat X Y D)] 
+    (.setTransform ctx a b c d e f))
+  ctx)
+
+(defn line-to! 
+  ([ctx [x y]]
+   (line-to! ctx origin [x y]))
+  ([ctx [x1 y1] [x2 y2]]
+   (doto ctx
+     (.beginPath)
+     (.moveTo x1 y1)
+     (.lineTo x2 y2)
+     (.stroke))
+   ctx))
+
+(defn stroke! [ctx]
+  (.stroke ctx)
+  ctx)
 
 (defn hello-world []
   [:div
    [:h1 (:text @app-state)]
-   [:h3 "Edit this and watch it change!"]])
+   [:h3 "Edit this and watch it change!"]
+   [canvas]])
 
 (reagent/render-component [hello-world]
                           (. js/document (getElementById "app")))
+
+; repl utils
+(defn ctx! []
+  (:ctx @app-state))
 
 (defn on-js-reload []
   ;; optionally touch your app-state to force rerendering depending on
