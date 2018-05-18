5b148494e6ff0861b525dc4e68105395cfc6ebfb
adz5a <antoine.d.zimmermann@gmail.com>
2018-05-18 14:09:47 +0200

    add protocol and rotate function

--- src/canvas/core.cljs
+++ src/canvas/core.cljs
@@ -3,8 +3,6 @@
 
 (enable-console-print!)
 
-(println "This text is printed from src/canvas/core.cljs. Go ahead and edit it and see reloading in action.")
-
 ;; define your app data so that it doesn't get over-written on reload
 
 (defonce app-state (atom {}))
@@ -30,13 +28,13 @@
 
 (def offsets (.getBoundingClientRect canvas))
 
-(swap! app-state assoc 
+(swap! app-state assoc
        :offset [(.-x offsets)
                 (.-y offsets)])
 
 (defonce on-resize (fn []
                      (let [offsets (.getBoundingClientRect canvas)]
-                       (swap! app-state assoc 
+                       (swap! app-state assoc
                               :offset [(.-x offsets)
                                        (.-y offsets)]))))
 
@@ -66,7 +64,7 @@
     (.clearRect ctx 0 0 w h)))
 
 
-(defn draw-line! 
+(defn draw-line!
   [line ctx]
   (let [[x y] line]
     (doto ctx
@@ -79,13 +77,56 @@
 
 (clear! ctx)
 (def l [100 0])
-(println (rotate l (/ PI 3)))
-(draw-line! (rotate l (/ PI 3)) ctx)
+
+(def PI|3 (/ PI 3))
+(def line-seq (list
+                (rotate l PI|3)
+                (rotate (rotate l PI|3) PI|3)))
+
+
+; (draw-line! (rotate l (/ PI 3)) ctx)
+
+(defprotocol IDrawable
+  (draw! [this ctx])
+  (draw-rotation! [this a ctx]))
+
+(defrecord Line [from direction]
+
+  IDrawable
+  (draw! [this ctx]
+    (let [{:keys [from direction]} this
+          [x0 y0] from
+          [x y] direction]
+      (doto ctx
+        (.moveTo x0 y0)
+        (.lineTo (+ x0 x) (+ y0 y))))
+    ctx)
+
+  (draw-rotation! [{:keys [from direction] :as this}
+                   a
+                   ctx]
+    (draw! (assoc this :direction (rotate direction a))
+           ctx)))
+
+(defn line
+  ([direction]
+   (->Line [0 0] direction))
+  ([from direction]
+   (->Line from direction)))
+
+(def start [50 50])
+(.beginPath ctx)
+
+(draw-rotation! 
+  (line [250 250] [100 100])
+  PI|3
+  ctx)
+
+(.stroke ctx)
+
 
 (defn on-js-reload []
   ;; optionally touch your app-state to force rerendering depending on
   ;; your application
   ;; (swap! app-state update-in [:__figwheel_counter] inc)
-
-  (println @app-state)
   )
